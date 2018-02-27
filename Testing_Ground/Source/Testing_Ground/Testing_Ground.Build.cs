811b3c5cbc8eb237d0292adaec0bca16ad9fa770
2014PC\Cesar Gonzalez <tankero@gmail.com>
2018-02-27 08:39:28 -0800

    no message

--- Testing_Ground/Source/Testing_Ground/Testing_Ground.Build.cs
+++ Testing_Ground/Source/Testing_Ground/Testing_Ground.Build.cs
@@ -8,6 +8,6 @@ public class Testing_Ground : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "AIModule", "GameplayTasks" });
 	}
 }
