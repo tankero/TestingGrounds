c0eaad738e22cbbfbb497fe9e7739e1fefe9586e
2014PC\Cesar Gonzalez <tankero@gmail.com>
2018-06-10 14:02:20 -0700

    More LFS/Git separation + IK

--- Testing_Ground/Source/Testing_Ground/Game_Mode/MainGameMode.cpp
+++ Testing_Ground/Source/Testing_Ground/Game_Mode/MainGameMode.cpp
@@ -9,7 +9,7 @@ AMainGameMode::AMainGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Static/Player/Behaviors/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Behavior/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
