ceff8d7954e8fa4334724eb04779d7be8ceada90
2014PC\Cesar Gonzalez <tankero@gmail.com>
2018-07-01 18:45:29 -0700

    Map refactor + fixes

--- Testing_Ground/Source/Testing_Ground/Game_Mode/MainGameMode.cpp
+++ Testing_Ground/Source/Testing_Ground/Game_Mode/MainGameMode.cpp
@@ -9,7 +9,7 @@ AMainGameMode::AMainGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Behavior/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Character/Behaviors/Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
